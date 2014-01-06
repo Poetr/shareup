e8a80744d5b699cf6b00a517992849da8736c24f
Peter Broers <P.Broers@outlook.com>
2014-01-06 20:20:56 +0100

    First commit

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,6 @@
+//= require jquery
+//= require jquery_ujs
+//= require angular
+//= require angular-route
+//= require angular-resource
+//= require_tree .
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Shareup::Application.config.secret_key_base = 'e6fa1c686f34c8ffce863fff798c8911407dfbdc8aad955220399f8a12a58cd898209ef941e018a0156ff4d7b7124ecde7597c52e7bdd5e0d0b7a2978ebf7646'
